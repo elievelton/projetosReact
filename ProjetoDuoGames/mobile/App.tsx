8abc5c83ecd36c08ca2a4d076ea02ff9d89f1e1c
elievelton <elievelton@gmail.com>
2022-09-20 17:02:23 -0300

    Alterações no server

--- ProjetoDuoGames/mobile/App.tsx
+++ ProjetoDuoGames/mobile/App.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import { Background } from './src/components/Background';
-import {useFonts, Inter_400Regular,Inter_600SemiBold,Inter_700Bold,Inter_900Black} from '@expo-google-fonts/inter'
-import {Home} from './src/screens/Home'
-import {Loading} from './src/components/Loading'
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter'
+import { Home } from './src/screens/Home'
+import { Loading } from './src/components/Loading'
 import { Game } from './src/screens/Game';
 
 
@@ -16,19 +16,19 @@ export default function App() {
 
   });
   return (
-<>
-   
-    <Background>      
-      <StatusBar 
-       barStyle='light-content'
-       backgroundColor='transparent'
-       translucent
-      />
-      {fontLoaded?<Home />:<Loading />}
+    <>
+
+      <Background>
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor='transparent'
+          translucent
+        />
+        {fontLoaded ? <Home /> : <Loading />}
       </Background>
-      
-      
-  </>
+
+
+    </>
   );
 }
 
--- ProjetoDuoGames/server/src/server.ts
+++ ProjetoDuoGames/server/src/server.ts
@@ -2,13 +2,28 @@ import express from "express";
 
 const app = express(); // Tudo está dentro de app apartir de agora
 
-app.get("/teste", (req, res) => {
+//criar anúncio
+app.post('/ads', (req, res) => {
+  return res.status(201).json([])
+})
+
+//listagem de games
+app.get('/game', (req, res) => {
+  return res.json([])
+})
+
+app.get('/ads/:id/discord', (req, res) => {
+  return res.json([])
+})
+
+app.get("/game/:id/ads", (req, res) => {
   return res.json([
     { id: 1, name: "Elievelton" },
     { id: 2, name: "Elievelton" },
     { id: 3, name: "Elievelton" },
     { id: 4, name: "Elievelton" },
   ]);
+
 });
 
 app.listen(3333, () => console.log("Servidor rodando local na porta 3333"));

10e79db6de8a2c57791d2b11cdd8ce5ef301a74c
elievelton <elievelton@gmail.com>
2022-09-21 02:09:06 -0300

    Api do backEnd sendo consumida

--- ProjetoDuoGames/server/src/server.ts
+++ ProjetoDuoGames/server/src/server.ts
@@ -1,28 +1,91 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client"; 
+import { convertMinutesToTime, convertTimeToMinutes } from "./utils/time";
+import cors from "cors";
 
 const app = express(); // Tudo está dentro de app apartir de agora
+app.use(express.json())
+app.use(cors())
+const prisma = new PrismaClient();
 
 //criar anúncio
-app.post('/ads', (req, res) => {
-  return res.status(201).json([])
+app.post('/games/:id/ads', async(req, res) => {
+
+  const gameId = req.params.id;
+  const body = req.body;
+const ad = await prisma.ad.create({
+  data:{
+    gameId,
+    name: body.name,
+    yearsPlaying: body.yearsPlaying,
+    discord: body.discord,
+    weekDays: body.weekDays.join(','),
+    hoursStart: convertTimeToMinutes(body.hoursStart),
+    hoursEnd: convertTimeToMinutes(body.hoursEnd),
+    useVoiceChannel: body.useVoiceChannel,
+
+  }
+})
+  return res.status(201).json(ad)
 })
 
 //listagem de games
-app.get('/game', (req, res) => {
-  return res.json([])
+app.get('/game', async (req, res) => {
+  const games = await prisma.game.findMany({
+    include:{
+      _count:{
+        select:{
+          ads: true,
+        }
+      }
+    }
+  })
+  return res.json(games)
 })
 
-app.get('/ads/:id/discord', (req, res) => {
-  return res.json([])
+app.get('/ads/:id/discord', async(req, res) => {
+
+  const adId = req.params.id;
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select:{
+      discord: true,
+    },
+    where: {
+      id: adId,
+    }
+  })
+  return res.json({
+    discord: ad.discord,
+  })
 })
 
-app.get("/game/:id/ads", (req, res) => {
-  return res.json([
-    { id: 1, name: "Elievelton" },
-    { id: 2, name: "Elievelton" },
-    { id: 3, name: "Elievelton" },
-    { id: 4, name: "Elievelton" },
-  ]);
+app.get("/game/:id/ads", async(req, res) => {
+  const gameId = req.params.id;
+  const ads = await prisma.ad.findMany({
+    select:{
+      id: true,
+      name:true,
+      weekDays:true,
+      useVoiceChannel:true,
+      yearsPlaying:true,
+      hoursStart:true,
+      hoursEnd:true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy:{
+      createdAt: 'desc',
+    }
+  })
+  return res.json(ads.map(ad =>{
+    return {
+      ...ad,
+      weekDays:ad.weekDays.split(','),
+      hoursStart:convertMinutesToTime(ad.hoursStart),
+      hoursEnd:convertMinutesToTime(ad.hoursEnd),
+    }
+  }));
 
 });
 
--- ProjetoDuoGames/web/src/App.tsx
+++ ProjetoDuoGames/web/src/App.tsx
@@ -1,81 +1,48 @@
 import './styles/main.css'
 import logosite from './assets/logo-nlw-esports.svg'
-import {MagnifyingGlassPlus} from "phosphor-react";
+import { MagnifyingGlassPlus } from "phosphor-react";
+import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
+import { useEffect, useState } from 'react';
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
 
 function App() {
-  return(
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/game').then((response) => response.json()).then(data => {
+      setGames(data);
+    });
+
+  }, []);
+
+  return (
     <>
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
-    <img src={logosite} />
-    <h2 className='text-6xl text-white font-black mt-16 bg-duo-gradient bg-clip-text'>Seu <span className='text-transparent bg-duo-gradient bg-clip-text'>duo </span>está aqui!</h2>
-    <div className='grid grid-cols-6 gap-6 mt-16 '>
+      <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+        <img src={logosite} />
+        <h2 className='text-6xl text-white font-black mt-16 bg-duo-gradient bg-clip-text'>Seu <span className='text-transparent bg-duo-gradient bg-clip-text'>duo </span>está aqui!</h2>
+        <div className='grid grid-cols-6 gap-6 mt-16 '>
 
-      <a href="" className='relative  rounded-lg overflow-hidden'>
-        <img src="./Apex.png" alt="" />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>Apex Legends</strong>
-          <span className='tex-zinc-300 text-sm text-[#2ecc71] block'>4 Anuncios</span>
-        </div>
-      </a>
-      <a href="" className='relative rounded-lg overflow-hidden' >
-        <img src="./League_of_legends.png"  />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>League of Legends</strong>
-          <span className='tex-zinc-300 text-sm text-[#2ecc71] block'>6 Anuncios</span>
-        </div>
-      </a>
-      <a href="" className='relative rounded-lg overflow-hidden' >
-        <img src="./CsGo.png"  />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>CsGo</strong>
-          <span className='tex-zinc-300 text-sm text-[#2ecc71] block'>6 Anuncios</span>
-        </div>
-      </a>
-      <a href="" className='relative rounded-lg overflow-hidden' >
-        <img src="./Dota2.png"  />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>Dota 2</strong>
-          <span className='tex-zinc-300 text-sm text-[#2ecc71] block'>6 Anuncios</span>
-        </div>
-      </a>
-      <a href="" className='relative rounded-lg overflow-hidden' >
-        <img src="./Ww.png"  />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>World of Warcraft</strong>
-          <span className='tex-zinc-300 text-sm text-[#2ecc71] block'>6 Anuncios</span>
-        </div>
-      </a>
-      <a href="" className='relative rounded-lg overflow-hidden' >
-        <img src="./fortnite.png"  />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>Fortnite</strong>
-          <span className='tex-zinc-300 text-sm text-[#2ecc71] block'>6 Anuncios</span>
-        </div>
-      </a>
+          {/* criado um componente */}
+          {games.map(game => {
+            return (
+              <GameBanner key = {game.id} bannerUrl={game.bannerUrl} title={game.title} adsCount={game._count.ads} />
+            )
+          })}
 
-    </div>
-    <div className='pt-1 bg-duo-gradient self-stretch rounded-lg mt-10 overflow-hidden'>
-    <div className='bg-[#2A2634] px-8 py-4 flex justify-between items-center'>
-      <div>
-        <strong className='text-2xl text-white font-black block py-1'>Não encontrou seu duo?</strong>
-        <span className='py-2 block'>
-        <span className='text-zinc-200 mt-6  px-2'>Publique um anúncio para encontrar novos 
-        </span>
-        <span className="before:block gap-6 before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
-        <span className="relative text-white gap-6 px-1 py-4 "> Players!</span>
-        
-         </span>
-         </span>       
-        
+        </div>
+        {/* componente para criar o anuncio */}
+        <CreateAdBanner />
       </div>
-      <button className='flex items-center gap-3 px-4 py-4 bg-violet-500 hover:bg-violet-600 text-white rounded '>
-        <MagnifyingGlassPlus size={26}/>
-        Publicar Anúncio
-      </button>
-    </div>
-    </div>
-    </div>
-    
+
     </>
   )
 }
